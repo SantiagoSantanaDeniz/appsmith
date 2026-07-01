cb54cdbf48a5ab2f670c69af2c668dc2bde13ee9
Santiago Santana <santi.santanaadeniz@gmail.com>
2026-07-01 17:08:51 +0000

    m

--- pages/Autenticacion/jsobjects/utils/utils.js
+++ pages/Autenticacion/jsobjects/utils/utils.js
@@ -1,50 +1,40 @@
 export default {
-
 	signIn: async () => {
-
 		const password = InputLoginPassword.text.trim();
-
 		const result = await loginUsuario.run();
-
 		const user = result?.[0];
-
 		if (!user) {
 			showAlert("Usuario no encontrado", "error");
 			return;
 		}
-
 		if (password === String(user.contrasenia).trim()) {
-
 			await storeValue("usuarioLogueado", user);
 
-			showAlert("Login correcto", "success");
+			const domain = "dev-2np4xfz0bw7lheit.us.auth0.com";
+			const clientId = "JEZzmfchqgtIDasxXr8BMJvd7EBwsTxO";
+			const redirectUri = encodeURIComponent("https://teti-grupo-10.appsmith.com/app/reporteincidentes/home-6a09e03b51fac745267a1ce7");
 
-			navigateTo("Home");
-
+			navigateTo(
+				`https://${domain}/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=openid%20profile%20email&acr_values=http://schemas.openid.net/policyidentifiers/mfa`,
+				{},
+				"SAME_WINDOW"
+			);
 		} else {
 			showAlert("Credenciales incorrectas", "error");
 		}
 	},
-
 	register: async () => {
-
 		const result = await createUsuario.run();
 		const user = result?.[0];
-
 		if (user) {
-
 			await storeValue("usuarioLogueado", user);
 			await storeValue("defaultTab", "Sign In");
-
 			showAlert("Usuario creado correctamente", "success");
-
 		} else {
 			showAlert("Error al crear usuario", "error");
 		}
 	},
-
 	setDefaultTab: async (newTab) => {
 		await storeValue("defaultTab", newTab);
 	}
-
-};
+}
